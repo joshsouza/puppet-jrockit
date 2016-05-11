ede20770ef6c357658f786823074afe386b3aad3
Josh Souza <jsouza@prokarma.com>
2016-05-11 11:54:43 -0600

    Lint fixes and any2bool instead of string booleans

--- manifests/installrockit.pp
+++ manifests/installrockit.pp
@@ -1,85 +1,88 @@
 # jrockit::instalrockit
 
-define jrockit::installrockit( 
-	$version        =  undef , 
-	$x64            =  undef,
-	$downloadDir    =  '/install/',
-	$puppetMountDir =  undef,
-	$installDemos   =  'false',
-	$installSource  =  'false',
-	$installJre     =  'true',
-	$setDefault     =  'true',
-	$jreInstallDir  =  '/usr/java',) {
+define jrockit::installrockit(
+  $version        =  undef ,
+  $x64            =  undef,
+  $downloadDir    =  '/install/',
+  $puppetMountDir =  undef,
+  $installDemos   =  false,
+  $installSource  =  false,
+  $installJre     =  true,
+  $setDefault     =  true,
+  $jreInstallDir  =  '/usr/java',) {
 
-	$fullVersion   =  "jrockit-jdk${version}"
-	$installDir    =  "/usr/java/${fullVersion}"
+  $fullVersion   =  "jrockit-jdk${version}"
+  $installDir    =  "/usr/java/${fullVersion}"
 
-	notify {"installrockit.pp ${title} ${version}":}
+  # Don't put notifies in your code, as they are not idempotent from a reporting perspective
+  #notify {"installrockit.pp ${title} ${version}":}
+  info("installrockit.pp: ${title} ${version}")
 
-	if $x64 == "true" {
-		$type = 'x64'
-	} else {
-		$type = 'ia32'
-	}
+  if any2bool($x64) {
+    $type = 'x64'
+  } else {
+    $type = 'ia32'
+  }
 
-	case $operatingsystem {
-		CentOS, RedHat, OracleLinux, Ubuntu, Debian: { 
-			$installVersion   = "linux"
-			$installExtension = ".bin"
-			$user             = "root"
-			$group            = "root"
-		}
-		windows: {
-			$installVersion   = "windows"
-			$installExtension = ".exe"
-		}
-		default: { 
-			fail("Unrecognized operating system") 
-		}
-	}
- 
-	$jdkfile =  "jrockit-jdk$version-${installVersion}-${type}${installExtension}"
+  case $::operatingsystem {
+    CentOS, RedHat, OracleLinux, Ubuntu, Debian: {
+      $installVersion   = 'linux'
+      $installExtension = '.bin'
+      $user             = 'root'
+      $group            = 'root'
+    }
+    windows: {
+      $installVersion   = 'windows'
+      $installExtension = '.exe'
+    }
+    default: {
+      fail('Unrecognized operating system')
+    }
+  }
 
-	File { 
-		replace => false,
-	}
+  $jdkfile =  "jrockit-jdk${version}-${installVersion}-${type}${installExtension}"
 
-	# check install folder
-	if ! defined(File[$downloadDir]) {
-		file { $downloadDir :
-			mode    => 0777,
-			path    => $downloadDir,
-			ensure  => directory,
-		}
-	}
+  File {
+    replace => false,
+  }
 
-	# if a mount was not specified then get the install media from the puppet master
+  # check install folder
+  if ! defined(File[$downloadDir]) {
+    file { $downloadDir :
+      ensure => directory,
+      mode   => '0777',
+      path   => $downloadDir,
+    }
+  }
+
+  # if a mount was not specified then get the install media from the puppet master
     if $puppetMountDir == undef {
-	    $mountDir = "puppet:///modules/jrockit/"    	
+      $mountDir = 'puppet:///modules/jrockit/'
     } else {
-    	$mountDir = $puppetMountDir
+      $mountDir = $puppetMountDir
     }
 
 
-	# download jdk to client
-	if ! defined(File["${downloadDir}${jdkfile}"]) {
-		file {"${downloadDir}${jdkfile}":
-			path    => "${downloadDir}${jdkfile}",
-			ensure  => present,
-			source  => "${mountDir}${jdkfile}",
-			require => File[$downloadDir],
-			mode    => 0777,
-		} 
-	}
+  # download jdk to client
+  if ! defined(File["${downloadDir}${jdkfile}"]) {
+    file {"${downloadDir}${jdkfile}":
+      ensure  => present,
+      path    => "${downloadDir}${jdkfile}",
+      source  => "${mountDir}${jdkfile}",
+      mode    => '0777',
+      require => File[$downloadDir],
+    }
+  }
 
-	# install on client 
-	javaexec {"jdkexec ${title} ${version}": 
-		path        => $downloadDir, 
-		fullversion => $fullVersion,
-		jdkfile     => $jdkfile,
-		setDefault  => $setDefault,
-		user        => $user,
-		group       => $group,
-		require     => File["${downloadDir}${jdkfile}"],
-	}
+  # install on client
+  jrockit::javaexec {"jdkexec ${title} ${version}":
+    version     => $version,
+    path        => $downloadDir,
+    fullversion => $fullVersion,
+    jdkfile     => $jdkfile,
+    setDefault  => $setDefault,
+    user        => $user,
+    group       => $group,
+    require     => File["${downloadDir}${jdkfile}"],
+  }
 }
